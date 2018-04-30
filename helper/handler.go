be84492c3da28d6f4203b75ab3d31a516cb20928
andrey.savchuk <andrey.savchuk@altarix.ru>
2018-05-01 00:09:17 +0300

    change auth method to outer

--- helper/handler.go
+++ helper/handler.go
@@ -8,7 +8,7 @@ import (
 
 type Handlers struct {
 	// ===== AUTH =====
-	Auth             func(structure.AuthRequest) (*structure.Auth, error)                            `method:"auth" inner:"true"`
+	Auth             func(structure.AuthRequest) (*structure.Auth, error)                            `method:"auth" inner:"false"`
 	GetUsers         func(structure.UsersRequest) (*structure.UsersResponse, error)                  `method:"get_users" inner:"true"`
 	CreateUpdateUser func(user libStr.AdminUser) (*libStr.AdminUser, error)                          `method:"create_update_user" inner:"true"`
 	DeleteUser       func(identities structure.IdentitiesRequest) (*structure.DeleteResponse, error) `method:"delete_user" inner:"true"`
