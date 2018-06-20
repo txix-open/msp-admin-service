b224f0d4a1bcf338f638221122e976c1778a211e
Andrey Savchuk <andrey.savchuk@altarix.ru>
2018-06-20 12:46:23 +0300

    add inner address into config change algorithm for creating method path

--- conf/remoteConfig.go
+++ conf/remoteConfig.go
@@ -3,7 +3,6 @@ package conf
 import "gitlab8.alx/msp2.0/msp-lib/database"
 
 type RemoteConfig struct {
-	GrpcPrefix       string                   `json:"grpcPrefix"`
 	GrpcAddress      AddressConfiguration     `valid:"required~Required" json:"grpcAddress"`
 	Database         database.DBConfiguration `valid:"required~Required" json:"database"`
 }
