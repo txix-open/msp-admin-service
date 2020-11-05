72b163e22de000a5ef09d1e9ba33a04f645e9f32
Рябов Владислав Олегович <riabovvo@elocont.ru>
2020-11-05 12:47:03 +0300

    Added real-time updates via websocket

--- conf/configuration.go
+++ conf/configuration.go
@@ -4,8 +4,9 @@ import "github.com/integration-system/isp-lib/v2/structure"
 
 type Configuration struct {
 	ConfigServiceAddress structure.AddressConfiguration
-	GrpcOuterAddress     structure.AddressConfiguration `valid:"required~Required" json:"grpcOuterAddress"`
-	GrpcInnerAddress     structure.AddressConfiguration `valid:"required~Required" json:"grpcInnerAddress"`
+	GrpcOuterAddress     structure.AddressConfiguration `valid:"required~Required"`
+	GrpcInnerAddress     structure.AddressConfiguration `valid:"required~Required"`
+	WebSocketAddress     structure.AddressConfiguration `valid:"required~Required"` //TODO : оставить поле - после модификации гейта, вернёмся к использованию
 	ModuleName           string
 	InstanceUuid         string
 }
