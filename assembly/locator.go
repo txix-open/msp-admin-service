4c3559bffdd76a312930281092523ab50b8c2f5d
novikovmu <novikovmu@elocont.ru>
2022-10-05 11:34:04 +0300

    MSP-1068 init assembly

---
+++ assembly/locator.go
@@ -0,0 +1,61 @@
+package assembly
+
+import (
+	"time"
+
+	"github.com/integration-system/isp-kit/db"
+	"github.com/integration-system/isp-kit/http/httpcli"
+	"github.com/integration-system/isp-kit/log"
+	"msp-admin-service/conf"
+	"msp-admin-service/controller"
+	"msp-admin-service/middleware"
+	"msp-admin-service/repository"
+	"msp-admin-service/service"
+
+	"github.com/integration-system/isp-kit/grpc/endpoint"
+	"github.com/integration-system/isp-kit/grpc/isp"
+	"msp-admin-service/routes"
+)
+
+type Locator struct {
+	logger  log.Logger
+	httpCli *httpcli.Client
+	db      db.DB
+}
+
+func NewLocator(logger log.Logger, httpCli *httpcli.Client, db db.DB) Locator {
+	return Locator{
+		logger:  logger,
+		httpCli: httpCli,
+		db:      db,
+	}
+}
+
+func (l Locator) Handler(cfg conf.Remote) isp.BackendServiceServer {
+	sudirRepo := repository.NewSudir(l.httpCli, cfg.SudirAuth)
+	roleRepo := repository.NewRole(l.db)
+	userRepo := repository.NewUser(l.db)
+
+	tokenService := service.NewToken(time.Second*time.Duration(cfg.ExpireSec), cfg.SecretKey)
+	sudirService := service.NewSudir(cfg.SudirAuth, sudirRepo, roleRepo)
+	userService := service.NewUser(tokenService, userRepo, roleRepo, l.logger)
+	authService := service.NewAuth(userRepo, tokenService, sudirService, l.logger)
+
+	userController := controller.NewUser(userService)
+	customizationController := controller.NewCustomization(cfg.UiDesign)
+	authController := controller.NewAuth(authService, l.logger)
+
+	handler := routes.Handler(
+		endpoint.DefaultWrapper(
+			l.logger,
+			middleware.ErrorHandler(l.logger),
+		),
+		routes.Controllers{
+			User:          userController,
+			Customization: customizationController,
+			Auth:          authController,
+		},
+	)
+
+	return handler
+}
