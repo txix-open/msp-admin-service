1db559469f8ad10a33f70cd5c438095d9e9e0aa2
Синельник Тимур Романович <sinelnik.timur@yandex.ru>
2026-07-29 11:49:58 +0300

    MSP-8172 Keep 1 example for hex color

--- domain/ui_design.go
+++ domain/ui_design.go
@@ -1,7 +1,6 @@
 package domain
 
-//nolint:lll
 type UIDesign struct {
 	Name         string // Название ui
-	PrimaryColor string `example:"#ff4d4f,#fa8c16,#a0d911,#1890ff,#722ed1,#d4b106,#e91e63,#ff5722,#795548,#abb8c3,#525252,#689f38"` // Цвет в HEX
+	PrimaryColor string `example:"#ff4d4f"` // Цвет в HEX
 }
